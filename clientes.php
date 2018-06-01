ec105e08e5d31e801c0c34557eb9a9e29ee40fda
RubioLlanos <paisacode@gmail.com>
2018-06-01 16:18:56 -0500

    Ajuste del index styles and html

--- clientes.php
+++ clientes.php
@@ -13,23 +13,23 @@
 				<input type="text" name="nombre">
 				<label for="nombre">Nombre: </label>
 			</div>
-				<div class="input-field">
+			<div class="input-field">
 				<input type="text" name="apellido">
 				<label for="apellido">Apellido: </label>
 			</div>
-				<div class="input-field">
+			<div class="input-field">
 				<input type="email" name="email">
 				<label for="email">Email: </label>
 			</div>
-				<div class="input-field">
+			<div class="input-field">
 				<input type="password" name="pass">
 				<label for="pass">Password: </label>
 			</div>
-				<div class="input-field">
+			<div class="input-field">
 				<input type="text" name="documento">
 				<label for="documento">Documento: </label>
 			</div>
-				<div class="input-field">
+			<div class="input-field">
 				<input type="text" name="direccion">
 				<label for="direccion">Dirección: </label>
 			</div>
@@ -44,7 +44,6 @@
 			<button class="btn">Enviar</button>
 		</form>
 	</div>
-
 <footer>
 	<script type="text/javascript" src="js/materialize.min.js"></script>	
 	<!--<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>-->
--- index.php
+++ index.php
@@ -7,79 +7,21 @@
 </head>
 <body>
 	<div class="container container-forms">
-		<h1 class="title_principal">Solicitud de Trámite(s) de Tránsito Virtual</h1>
-		<div class="card">
-			<div class="card-content">
-				<div class="row">
-					<div class="col s12">
-						<a href="clientes.php" class="btn">Registrar Cliente</button>
-						<h1 class="card-title">!Hola, ingresa tu e-mail</h1>
-						<form action="#" id="login">
-							<div class="input-field">
-								<input type="Email" id="nombre">
-								<label for="nombre">Email:</label>
-							</div>							
-							<div id="content-pass">
-								<div class="input-field">
-									<input type="password" name="password">
-									<label for="pass">Contraseña:</label>
-								</div>
-								<a class="btn btn-large red" id="next1" href="">Ingresar</a>
-							</div>
-							<div class="center-align">
-								<a href="">Crear cuenta</a>
-							</div>
-						</form>						
-					</div>
-					<section id="datos-pers">
-						<div class="col s12">
-							<h1 class="title_principal">Completa tus datos por favor!!!</h1>
-						<form>
-							<div class="col s6">
-								<div class="input-field">
-									<input type="text" name="nombre">
-									<label for="nombre">Nombre:</label>
-								</div>
-								<div class="input-field">
-									<input type="text" name="nombre">
-									<label for="nombre">Apellido:</label>
-								</div>
-								<div class="input-field">
-									<input type="text" name="nombre">
-									<label for="nombre">Email:</label>
-								</div>
-								<div class="input-field">
-									<input type="text" name="nombre">
-									<label for="nombre">Contraseña:</label>
-								</div>
-							</div>
-							<div class="col s6">
-								<div class="input-field">
-									<input type="number" name="nombre">
-									<label for="nombre">Numero de documento:</label>
-								</div>
-								<div class="input-field">
-									<input type="text" name="direccion">
-									<label for="direccion">Dirección:</label>
-								</div>
-								<div class="input-field">
-									<input type="text" name="nombre">
-									<label for="nombre">Teléfono</label>
-								</div>
-								<div class="input-field">
-									<input type="text" name="ciudad">
-									<label for="ciudad">Ciudad</label>
-								</div>								
-							</div>
-							<div class="col s12">
-								<a class="btn btn-large red" href="">Continuar</a>
-							</div>
-						</form>						
-						</div>
-					</section>
-				</div>
+		<h3>Index de la página</h3>
+		<form method="post" action="">
+			<div class="input-field">
+				<input type="email" name="email" required>
+				<label for="email">Email:</label>
 			</div>
-		</div>
+			<div class="input-field">
+				<input type="password" name="pass">
+				<label for="pass">Contraseña:</label>
+			</div>
+			<a href="clientes.php" class="btn">Ingresar</a>
+		</form>
+		<p>¿No tienes una cuenta?<a href="clientes.php" class="">Crear cuenta</a></p>
+		
+		
 	</div>
 
 <footer>
